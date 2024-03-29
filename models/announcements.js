914d0bf53ef5d4f8adc5877f7eccf78de7114b1e
NNK-KALI <neerajkumar18971@gmail.com>
2024-03-29 14:21:36 +0530

    announcement model

---
+++ models/announcements.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+
+const announcementSchemaMongoose =  new mongoose.Schema({
+  title:  {
+    type: String,
+    minlength: 1,
+    required: true
+  },
+  publishedAt: { 
+    type: Date,
+    default: Date.now   
+  },
+  content: {
+    type: String,
+  }
+});
+
+const Announcement =  mongoose.model("Announcement", announcementSchemaMongoose);
+
+const announcementSchemaJoi = Joi.object({
+  title: Joi.string().min(1).required(),
+  content: Joi.string()
+});
+
+function validateAnnouncement(announcement) {
+  return announcementSchemaJoi.validate(announcement);
+}
+
+module.exports.Announcement = Announcement;
+module.exports.validateAnnouncement = validateAnnouncement;
